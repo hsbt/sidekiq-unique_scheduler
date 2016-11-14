ef78830b318405fb337e4e2c160c16dbb6e2a6f3
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2016-11-14 18:40:56 +0900

    added test case with dirty exit node sessions

--- test/sidekiq/unique_scheduler_test.rb
+++ test/sidekiq/unique_scheduler_test.rb
@@ -17,4 +17,14 @@ class Sidekiq::UniqueSchedulerTest < Minitest::Test
     Diplomat::Session.expects(:destroy).once
     Sidekiq::UniqueScheduler.unlock
   end
+
+  def test_unique_scheduler_lock_with_dirty_exit
+    Diplomat::Session.expects(:list).returns([{'Node' => Socket.gethostname.chomp + "-2", 'Name' => "sidekiq-unique_scheduler"}]).once
+    Diplomat::Health.expects(:node).with(Socket.gethostname.chomp + "-2").returns([{'ID' => 'foo', 'Status' => 'critical'}]).once
+    Diplomat::Session.expects(:destroy).with(any_parameters).once
+
+    Diplomat::Session.expects(:create).returns('bar').once
+    Diplomat::Lock.expects(:acquire).with("/sidekiq-unique_scheduler/lock", 'bar').returns(true).once
+    assert Sidekiq::UniqueScheduler.lock
+  end
 end
