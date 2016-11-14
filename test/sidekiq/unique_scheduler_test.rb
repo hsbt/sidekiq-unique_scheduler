8f2b3f030464376a6597252a5a991a5fced11331
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2016-11-14 18:21:57 +0900

    added basic tests for lock and unlock

--- test/sidekiq/unique_scheduler_test.rb
+++ test/sidekiq/unique_scheduler_test.rb
@@ -4,4 +4,17 @@ class Sidekiq::UniqueSchedulerTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Sidekiq::UniqueScheduler::VERSION
   end
+
+  def test_unique_scheduler_lock
+    Diplomat::Session.expects(:list).returns([]).once
+    Diplomat::Session.expects(:create).returns('foo').once
+    Diplomat::Lock.expects(:acquire).with("/sidekiq-unique_scheduler/lock", 'foo').returns(true).once
+    assert Sidekiq::UniqueScheduler.lock
+  end
+
+  def test_unique_scheduler_unlock
+    Diplomat::Session.expects(:list).returns([{'Node' => Socket.gethostname.chomp, 'Name' => "sidekiq-unique_scheduler"}]).once
+    Diplomat::Session.expects(:destroy).once
+    Sidekiq::UniqueScheduler.unlock
+  end
 end
